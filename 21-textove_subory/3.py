402923e2534fe720251a3b02bb3f5acd2a9b9b18
PetraSkolova <petraskolova13@gmail.com>
2024-06-18 16:27:42 +0200

    idk co som urobila xd,aj tak to nefunguje hah

--- 21-textove_subory/3.py
+++ 21-textove_subory/3.py
@@ -5,10 +5,35 @@ canvas.pack()
 
 fbody = open('body.txt', 'r')
 for riadok in fbody:
+
+    medzera = riadok.find(' ')
+    tvar = riadok[ :medzera]
+    riadok = riadok[medzera+1]
+
     medzera = riadok.find(' ')
     x = int(riadok[:medzera])
+    riadok = riadok[medzera+1:]
+
+    medzera = riadok.find(' ')
     y = int(riadok[medzera:])
-    print(x, y)
-    canvas.create_oval()
+    riadok = riadok[medzera+1:]
+
+    medzera = riadok.find(' ')
+    r = int(riadok[:medzera])
+    riadok = riadok[medzera+1:]
+
+    medzera = riadok.find(' ')
+    g = int(riadok[:medzera])
+    riadok = riadok[medzera+1:]
+
+    medzera = riadok.find(' ')
+    b = int(riadok[:medzera])
+    riadok = riadok[medzera+1:]
+
+    print(tvar, x, y)
+    if tvar == 'r':
+        canvas.create_rectangle(x, y, x + 20, y + 20)
+    else:
+        canvas.create_oval(x, y, x + 20, y + 20)
 
 tkinter.mainloop()
